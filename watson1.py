039f6560ecbcb6d60627273b22900e91a523ed25
Patrick Di justo <patrick@dijusto.com>
2018-08-09 16:16:08 -0400

    something new has been added

--- watson1.py
+++ watson1.py
@@ -48,6 +48,9 @@ def getCurrent(pastNumber):
 	## Read Trump's most recent tweet
 	## Extract Tweet ID Number
 
+	putput=[]
+	
+	
 	St = api.GetUserTimeline(0,"realDonaldTrump",pastNumber,0,1)
 	
 	writeLog("Gotten current: ", 1, "a")
@@ -57,15 +60,35 @@ def getCurrent(pastNumber):
 		
 	print("And now just the first entry")
 	print (St[0].id)
-		
-	print("and now just the text")
-	mid_text = normalize('NFKD', St[0].text).encode('ascii','ignore')
+	putput.append(St[0].id)
+
+
 	
-	out_text = re.sub(r'https://\S+', '', mid_text)
+	print("and now just the text")
+	# mid_text = normalize('NFKD', St[0].text).encode('ascii','ignore')
+	
+	# midi_text = re.sub(r'https://\S+', '', mid_text)
+	
+	# out_text = re.sub(r'@\S+', '', midi_text)
+	
+	# mid_text = normalize('NFKD', St[0].text).encode('ascii','ignore')
+	
+	# midi_text = re.sub(r'https://\S+', '', normalize('NFKD', St[0].text).encode('ascii','ignore'))
+	
+	mid_text = re.sub(r'@\S+', '', re.sub(r'https://\S+', '', normalize('NFKD', St[0].text).encode('ascii','ignore')))
+	
+	out_text = mid_text.lstrip()
+	
 	
 	print(out_text)
-		
-	return St[0].id
+	putput.append(out_text)
+
+	
+	return putput
+	
+	
+	
+	
 
 def writePast(ccc):
         flx = open('pastNumber.txt', "w")
@@ -92,9 +115,11 @@ print ("Past number is:")
 
 print (pastNumber)
 
-currentNumber = getCurrent(pastNumber)
+outlist = getCurrent(pastNumber)
 
-writePast(currentNumber)
+print(outlist)
+
+writePast(outlist[0])
 
 
 # if (currentNumber > pastNumber):
@@ -115,16 +140,16 @@ writePast(currentNumber)
 		
 		
 		
-# tone_analyzer = ToneAnalyzerV3(
-    # version='2017-09-21',
-    # username='419a0281-d84a-4281-bc03-3def84761f7f',
-    # password='aTC4VyXndf2v'
-# )
+tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+    username='419a0281-d84a-4281-bc03-3def84761f7f',
+    password='aTC4VyXndf2v'
+)
 
 
-# tone_analyzer.set_url('https://gateway.watsonplatform.net/tone-analyzer/api')
+tone_analyzer.set_url('https://gateway.watsonplatform.net/tone-analyzer/api')
 
-# tone_analyzer.set_detailed_response(False)
+tone_analyzer.set_detailed_response(False)
 
 
 
@@ -136,19 +161,19 @@ writePast(currentNumber)
 
 
 # text = 'Team, I know that times are tough! Product sales have been disappointing for the past three quarters. We have a competitive product, but we need to do a better job of selling it!'
-# content_type = 'application/json'
+content_type = 'application/json'
 
 # ##text = "When in the course of human events, it becomes necessary to one people to dissolve the political bands which have connected them with another, a decent respect for the opinions of mankind compels them to list the reasons for their separation."
 
 # # content_type = 'text/plain'
 
-# try:
-	# tone = tone_analyzer.tone({"text": text},content_type, True)
-# except WatsonApiException as ex:
-# #except:
-    # print "Method failed with status code " + str(ex.code) + ": " + ex.message
-	# #print("Failure!")
+try:
+	tone = tone_analyzer.tone({"text": outlist[1]},content_type, True)
+except WatsonApiException as ex:
+#except:
+    print "Method failed with status code " + str(ex.code) + ": " + ex.message
+	#print("Failure!")
 
 
-# ##print(tone)	
-# print(json.dumps(tone, indent=2))
+##print(tone)	
+print(json.dumps(tone, indent=2))
