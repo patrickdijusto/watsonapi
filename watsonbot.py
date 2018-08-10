45312cf392b2a3f498c7f0013bc589e9e35e7229
Patrick Di justo <patrick@dijusto.com>
2018-08-10 16:18:15 -0400

    Allllllmost there

--- watsonbot.py
+++ watsonbot.py
@@ -11,32 +11,66 @@ global api
 ## Run entire twitter infrasctucture
 
 global handler
-
-handler = raw_input('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nInput a twitter handle:')
-
-print(handler)
+handler = "botrandrussell"
 
 
 
-print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nestablish the twitter object')
-# see "Authentication" section below for tokens and keys
-api = twitter.Api(consumer_key=CONSUMER_KEY,
+def estapi():
+	print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nestablish the twitter object')
+	# see "Authentication" section below for tokens and keys
+	twapi = twitter.Api(consumer_key=CONSUMER_KEY,
 	consumer_secret=CONSUMER_SECRET,
 	access_token_key=OAUTH_TOKEN,
 	access_token_secret=OAUTH_SECRET,
     )
+	print('twitter object established')
+	return twapi
 
-print('twitter object established')
+	
+def getMentions():
+	
+	namename=[]
+	ment = api.GetMentions(count=1)
+	print("\n\n\nLast mention:")
+	
+	##print(json.dumps("["+ment[0]+"]", indent=2))
 
+	
+	
+	
+	print(ment[0])
+	print("\n\n")
+	reply = ment[0].user.screen_name
+	print("Reply to: %s\n\n" % reply)
+	#print(ment[0].text)
+	
+	handler = re.sub(r'@\S+', '', ment[0].text).lstrip()
+	
+	# print(ment[1])
+	print("\n\n")
+	print(handler)
+	# print("\n\n\n\n\n\n\n\n\n")
+	
+	# stat=api.GetStatus(ment[0])
+	# print("\n\n\nLast status:")
+	# print(stat)
+	# print("\n\n\n\n\n\n\n\n\n")
+	namename.append(reply)
+	namename.append(handler) 
+	
+	return namename
+	
+	
+	
 def getPast():
 	## open file
 	## 	Read # of previous tweet
 	## Close file?
 
 	try:
-		flx = open('pastNumber.txt',"r")
+		flx = open('mypastNumber.txt',"r")
 	except:
-		flx = open('pastNumber.txt',"w")
+		flx = open('mypastNumber.txt',"w")
 		flx.write("0")
 		flx.close()
 		flx = open('pastNumber.txt',"r")
@@ -50,7 +84,7 @@ def getPast():
 	               
 	return int(row)
 
-def getCurrent(pastNumber,onameka):
+def getCurrent(onameka):
 	
 	##Contact twitter
 	## Read Trump's most recent tweet
@@ -59,12 +93,14 @@ def getCurrent(pastNumber,onameka):
 	putput=[]
 	
 	
-	St = api.GetUserTimeline(0,onameka,pastNumber,0,1)
+	##St = api.GetUserTimeline(0,onameka,pastNumber,0,1)
+	
+	St = api.GetUserTimeline(screen_name=onameka,count=1)
 	
 	writeLog("Gotten current: ", 1, "a")
 
-	# print("And now we print the Status of the last Tweet")
-	# print(St)
+	print("And now we print the Status of the last Tweet")
+	print(St)
 		
 	# print("And now just the first entry")
 	# print (St[0].id)
@@ -99,7 +135,7 @@ def getCurrent(pastNumber,onameka):
 	
 
 def writePast(ccc):
-        flx = open('pastNumber.txt', "w")
+        flx = open('mypastNumber.txt', "w")
         flx.write(str(ccc))
         flx.close()
         writeLog("writing past: ", int(ccc), "a")
@@ -117,24 +153,7 @@ def writeLog(TweetText, currentNumber, mode):
         fly.write(str(currentNumber))
         fly.close()
 
-pastNumber = getPast()
-
-##print ("Past number is:")
-
-##print (pastNumber)
-
-
-##outlist = getCurrent(pastNumber,handler)
-outlist = getCurrent(0,handler)
-
-##print(outlist)
-
-writePast(outlist[0])
-
-
-# if (currentNumber > pastNumber):
-        # postReply(TweetText, currentNumber)
-						
+		
 
 ##  THE WATSON SECTION OF THE CODE
 ##  https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/python.html?python#tone
@@ -145,66 +164,110 @@ writePast(outlist[0])
 # 2016-05-19: The service can return results for the following tone IDs of the different categories: for the emotion category: anger, disgust, fear, joy, and sadness; for the language category: analytical, confident, and tentative; for the social category: openness_big5, conscientiousness_big5, extraversion_big5, agreeableness_big5, and emotional_range_big5. The service returns scores for all tones of a category, regardless of their values.
 						
 						
+def watsonapi():						
 						
-						
-tone_analyzer = ToneAnalyzerV3(
-    ##version='2017-09-21',
-	version='2016-05-19',
+	tone_analyzer = ToneAnalyzerV3(
+    version='2017-09-21',
+	##version='2016-05-19',
     username='419a0281-d84a-4281-bc03-3def84761f7f',
     password='aTC4VyXndf2v'
-)
+	)
 
 
-tone_analyzer.set_url('https://gateway.watsonplatform.net/tone-analyzer/api')
+	tone_analyzer.set_url('https://gateway.watsonplatform.net/tone-analyzer/api')
 
-tone_analyzer.set_detailed_response(False)
+	tone_analyzer.set_detailed_response(False)
 
-content_type = 'application/json'
-
-
-try:
-	if(outlist[1][:2] != "RT"):
-		tone = tone_analyzer.tone({"text": outlist[1]},content_type, True)
-except WatsonApiException as ex:
-#except:
-    print "Method failed with status code " + str(ex.code) + ": " + ex.message
-	#print("Failure!")
+	content_type = 'application/json'
 
 
-print(json.dumps(tone, indent=2))
-
-print(tone)	
-
-try:
-	t0=(tone[u'document_tone'][u'tones'][0][u'tone_name'])
-	s0=(tone[u'document_tone'][u'tones'][0][u'score'])
-	print("\nThis tweet gets %s out of 100 for %s" % ((s0* 100),t0))
-except:
-	print('\nNothing to report')
+	try:
+		trone = tone_analyzer.tone({"text": outlist[1]},content_type, True)
+	except WatsonApiException as ex:
+	#except:
+		print "Method failed with status code " + str(ex.code) + ": " + ex.message
+		#print("Failure!")
 
 
-try:	
-	t1=(tone[u'document_tone'][u'tones'][1][u'tone_name'])
-	s1=(tone[u'document_tone'][u'tones'][1][u'score'])
-	print("This tweet gets %s out of 100 for %s" % ((s1* 100),t1))
-except:
-	print('Nothing further to report')
-	
-	
-	
-try:
-	t2=(tone[u'document_tone'][u'tones'][2][u'tone_name'])
-	s2=(tone[u'document_tone'][u'tones'][2][u'score'])
-	print("This tweet gets %s out of 100 for %s" % ((s2* 100),t2))
-except:
-	print('Nothing further to report')
+	print(json.dumps(trone, indent=2))
+
+	##print(tone)	
+
+	return trone
 	
 
-try:	
-	t3=(tone[u'document_tone'][u'tones'][3][u'tone_name'])
-	s3=(tone[u'document_tone'][u'tones'][3][u'score'])
-	print("This tweet gets %s out of 100 for %s" % ((s3* 100),t3))
-except:
-	print('Nothing further to report')
+	
+def parser(tone):	
+	
+	result=""
+	
+	try:
+		t0=(tone[u'document_tone'][u'tones'][0][u'tone_name'])
+		s0=(tone[u'document_tone'][u'tones'][0][u'score'])
+		result+="\nThis tweet gets %s out of 100 for %s" % ((s0* 100),t0)
+	except:
+		result+=('\nNothing to report')
 
 
+	try:	
+		t1=(tone[u'document_tone'][u'tones'][1][u'tone_name'])
+		s1=(tone[u'document_tone'][u'tones'][1][u'score'])
+		result.append("This tweet gets %s out of 100 for %s" % ((s1* 100),t1))
+	except:
+		print('Nothing further to report')
+		
+		
+		
+	try:
+		t2=(tone[u'document_tone'][u'tones'][2][u'tone_name'])
+		s2=(tone[u'document_tone'][u'tones'][2][u'score'])
+		result.append("This tweet gets %s out of 100 for %s" % ((s2* 100),t2))
+	except:
+		print('Nothing further to report')
+		
+
+	try:	
+		t3=(tone[u'document_tone'][u'tones'][3][u'tone_name'])
+		s3=(tone[u'document_tone'][u'tones'][3][u'score'])
+		result+="This tweet gets %s out of 100 for %s" % ((s3* 100),t3)
+	except:
+		print('Nothing further to report')
+		
+	return result	
+
+
+def answer(names, text):
+
+	if(text =="RT"):
+		reply ="@"+names[0]+ " : The last tweet from "+names[1]+" was a retweet and therefore unsuitable for analysis."
+	else:
+		reply = "@"+names[0]+ " : The last tweet from "+names[1]+" gets: \n"+text
+	
+	print(reply)
+	api.PostUpdate(reply)
+	
+		
+		
+		
+		
+api = estapi()		
+
+names= getMentions()
+		
+# pastNumber = getPast()
+
+# ##print ("Past number is:")
+
+# ##print (pastNumber)
+
+outlist = getCurrent(names[1])
+
+# ##print(outlist)
+
+# writePast(outlist[0])
+
+if(outlist[1][:2] != "RT"):
+	results = parser(watsonapi())
+	answer(names, results)
+else:
+	answer(names, "RT")
